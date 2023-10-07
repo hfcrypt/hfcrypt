8800f9463b619fb396d6645119c0c0a3938c07f1
mrfakename <me@mrfake.name>
2023-10-07 15:06:59 -0700

    Fix reqs

--- template/hfcryptdecrypt.sh
+++ template/hfcryptdecrypt.sh
@@ -1,3 +1,3 @@
 pip3 install cryptography --user && python3 decrypt.py
-python3 -m pip install --no-cache-dir -r requirements.txt --user
+python3 -m pip install --no-cache-dir -r hfapp/requirements.txt --user
 python3 hfapp/app.py
